6abd6c8d6e5e068a6c2383cc9beb815362761603
Krishan17212 <kkprabhakar1008@gmail.com>
2026-05-31 17:32:31 +0530

    Solved inverted right triangle problem with for loop

--- Loops/forLoop.js
+++ Loops/forLoop.js
@@ -24,4 +24,16 @@ function rightTriangle(num) {
   }
 }
 
-rightTriangle(5);
+// rightTriangle(5);
+
+function invertedRightTriangle(num) {
+  for (let i = num; i >= 1; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+
+invertedRightTriangle(5);
