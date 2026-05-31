3d40d449e073264d6be3d6742a5652c1ac5484e6
Krishan17212 <kkprabhakar1008@gmail.com>
2026-05-31 16:39:19 +0530

    Started loops with for loop and solved square pattern

---
+++ Loops/forLoop.js
@@ -0,0 +1,13 @@
+// Square Pattern
+
+function squarePatter(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= num; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+
+squarePatter(5);
