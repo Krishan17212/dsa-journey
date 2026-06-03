38713307887c76728a3fe6dfd9903d497d7c3eb1
Krishan17212 <kkprabhakar1008@gmail.com>
2026-06-03 12:40:48 +0530

    solved two sum with brute force

---
+++ Objects/twoSum.js
@@ -0,0 +1,23 @@
+// Two Sum
+// Given an array of integers nums and an integer target,
+// Return indices of the two numbers such that they add up to target.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+function twoSum(nums, target) {
+  // O(n^2)
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+// console.log(twoSum([2, 7, 11, 15], 9));
